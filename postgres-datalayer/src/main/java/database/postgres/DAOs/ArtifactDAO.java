64b0ac5c772d4cb923461aee35d9ff0e09f13a89
Esben-Andreas-Madsen <mainmaxen@gmail.com>
2025-05-26 12:41:39 +0200

    todo

--- postgres-datalayer/src/main/java/database/postgres/DAOs/ArtifactDAO.java
+++ postgres-datalayer/src/main/java/database/postgres/DAOs/ArtifactDAO.java
@@ -5,6 +5,7 @@ import DTOs.Artifact;
 
 public interface ArtifactDAO {
     Artifact createArtifact(Artifact artifact);
+    // TODO return artifact instead by utilizing mappers
     ArtifactProto getArtifactById(int id);
     ArtifactProto getArtifactByName(String name);
     void updateArtifact(ArtifactProto artifact);
