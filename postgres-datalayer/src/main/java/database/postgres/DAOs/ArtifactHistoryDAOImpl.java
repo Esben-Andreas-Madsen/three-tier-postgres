8ba0215442d2448d97db62685fd5f740d86fe0e0
Esben-Andreas-Madsen <mainmaxen@gmail.com>
2025-05-26 13:03:13 +0200

    Update ArtifactHistoryDAOImpl.java

--- postgres-datalayer/src/main/java/database/postgres/DAOs/ArtifactHistoryDAOImpl.java
+++ postgres-datalayer/src/main/java/database/postgres/DAOs/ArtifactHistoryDAOImpl.java
@@ -1,4 +1,25 @@
 package database.postgres.DAOs;
 
+import DTOs.ArtifactHistory;
+
 public class ArtifactHistoryDAOImpl implements ArtifactHistoryDAO {
+    @Override
+    public ArtifactHistory createArtifactHistory(ArtifactHistory artifactHistory, String artifactName) {
+        return null;
+    }
+
+    @Override
+    public ArtifactHistory getArtifactHistoryById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateArtifactHistory(int id) {
+
+    }
+
+    @Override
+    public void deleteArtifactHistory(int id) {
+
+    }
 }
